6c339a93934350e1102ae4d9b9a63bfd65585624
Thomas Tursics <thomas@tursics.de>
2025-07-13 14:53:02 +0200

    add a self test system

--- assets/index.js
+++ assets/index.js
@@ -2,7 +2,46 @@ var config = {
     debug: true,
 };
 
+function areSameBoards(left, right) {
+    var l = left.length;
+    var r = right.length;
+
+    if (l !== r) {
+        if (config.debug) {
+            console.debug('Length mismatch');
+        }
+        return false;
+    }
+
+    for (var i = 0; i < l; ++i) {
+        if (left[i] !== right[i]) {
+            if (config.debug) {
+                console.debug(left[i]);
+                console.debug(right[i]);
+            }
+            return false;
+        }
+    }
+    return true;
+}
+
 function selfTest() {
+    var repositories = level.getTests();
+
+    repositories.forEach(repository => {
+        var ret = solve.board(repository);
+        var success = areSameBoards(ret.initial, repository.design) && areSameBoards(ret.cleaned, repository.expectedResult) && areSameBoards(ret.animate, repository.expectedAnimation);
+
+        if (!success) {
+            console.error('Test "' + repository.title + '" failed.');
+            console.table(ret.initial);
+            console.table(ret.cleaned);
+            console.table(ret.animate);
+        }
+    });
+}
+
+function selfDebug() {
     var repository = level.getDebug();
 
     var ret = solve.board(repository);
@@ -17,4 +56,5 @@ window.onload = function() {
     div.style.display = 'none';
 
     selfTest();
+    selfDebug();
 }
