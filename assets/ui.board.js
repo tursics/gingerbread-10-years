0ed103e1ed0a6e4e7ef225719517b0d160012e4f
Thomas Tursics <thomas@tursics.de>
2025-08-15 07:45:23 +0200

    center board

--- assets/ui.board.js
+++ assets/ui.board.js
@@ -8,7 +8,6 @@ var uiBoard = (function () {
         if (null === div) {
             div = document.createElement('div');
             div.id = boardID;
-            div.style.cssText = 'position:absolute;width:100%;height:100%;background:#222;';
             document.body.appendChild(div);
         }
 
@@ -20,8 +19,10 @@ var uiBoard = (function () {
         var cols = board.getCols(repository);
         var div = null;
 
-        boardDIV.style.width = cols + 'em';
         boardDIV.style.height = rows + 'em';
+        boardDIV.style.width = cols + 'em';
+        boardDIV.style.left = (-cols / 2) + 'em';
+        boardDIV.style.top = (-rows / 2) + 'em';
 
         for (var y = 0; y < rows; ++y) {
             for (var x = 0; x < cols; ++x) {
